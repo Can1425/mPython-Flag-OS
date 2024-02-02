c2d5d8b92f0793dfb50fed81ae605c36ed93bc16
惟灿 Can1425 <myqf1425@126.com>
2024-02-02 06:03:30 +0000

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,7 @@
 export default defineAppConfig({
   docus: {
     title: 'Flag OS Develop',
+    titleTemplate: 'Flag OS Develop',
     description: 'Together with Flag OS, build a complete ecological experience of mPython.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
